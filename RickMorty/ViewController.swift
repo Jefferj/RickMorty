e9cf017340b9e6fc48840fdd2c79d57b285f1783
Jefferj <jefferj080@gmail.com>
2023-01-11 14:56:44 -0500

    Add project

---
+++ RickMorty/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by Jefferson Naranjo rodríguez on 11/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
