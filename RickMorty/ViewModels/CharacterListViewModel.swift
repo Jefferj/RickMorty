60978b2851938629c5ebea609cc08930cc9c6fa7
Jefferj <jefferj080@gmail.com>
2023-01-19 16:37:53 -0500

    Completed view characters

--- RickMorty/ViewModels/CharacterListViewModel.swift
+++ RickMorty/ViewModels/CharacterListViewModel.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
+protocol RMCharacterListViewModelDelegate: AnyObject {
+    func didLoadInitialCharacters()
+}
+
 final class CharacterListViewModel: NSObject {
-    func fetchCharacters() {
-        RMService.shared.execute(.listCharactersRequest, expecting: RMGetCharactersResponse.self) { result in
+    
+    public weak var delegate: RMCharacterListViewModelDelegate?
+    
+    private var characters: [RMCharacter] = [] {
+        didSet{
+            for character in characters {
+                let viewModel = RMCharacterCollectionViewCellViewModel(characterName: character.name,
+                                                                       characterStatus: character.status,
+                                                                       characterImageUrl: URL(string: character.image)
+                )
+                cellViewModels.append(viewModel)
+            }
+        }
+    }
+    private var cellViewModels: [RMCharacterCollectionViewCellViewModel] = []
+    public func fetchCharacters() {
+        RMService.shared.execute(.listCharactersRequest, expecting: RMGetCharactersResponse.self) { [weak self] result in
             switch result {
-            case .success(let model):
-                print("Example image url: "+String(model.results.first?.image ?? "No image"))
+            case .success(let responseModel):
+                let results = responseModel.results
+                self?.characters = results
+                DispatchQueue.main.async {
+                    self?.delegate?.didLoadInitialCharacters()
+                }
             case .failure(let error):
                 print(String(describing: error))
             }
@@ -22,17 +45,14 @@ final class CharacterListViewModel: NSObject {
 
 extension CharacterListViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return cellViewModels.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterCollectionViewCell else {
             fatalError("Unsupported cell")
         }
-        let viewModel = RMCharacterCollectionViewCellViewModel(characterName: "Afraz",
-                                                               characterStatus: .alive,
-                                                               characterImageUrl: URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"))
-        cell.configure(with: viewModel)
+        cell.configure(with: cellViewModels[indexPath.row])
         return cell
     }
     
--- RickMorty/Views/CharacterListView.swift
+++ RickMorty/Views/CharacterListView.swift
@@ -33,6 +33,7 @@ final class CharacterListView: UIView {
         addSubviews(collectionView, spinner)
         addConstraints()
         spinner.startAnimating()
+        viewModel.delegate = self
         viewModel.fetchCharacters()
         setUpCollectionView()
     }
@@ -57,12 +58,17 @@ final class CharacterListView: UIView {
     private func setUpCollectionView(){
         collectionView.dataSource = viewModel
         collectionView.delegate = viewModel
-        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
-            self.spinner.stopAnimating()
-            self.collectionView.isHidden = false
-            UIView.animate(withDuration: 0.4){
-                self.collectionView.alpha = 1
-            }
-        })
+        
+    }
+}
+
+extension CharacterListView: RMCharacterListViewModelDelegate {
+    func didLoadInitialCharacters(){
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData()
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
     }
 }
